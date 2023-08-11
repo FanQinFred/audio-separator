f6ea9a2ce0e8440bc9d041bfddb32a81058ab057
FanQinFred <56213127+FanQinFred@users.noreply.github.com>
2023-08-12 00:32:07 +0800

    1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 -f https://download.pytorch.org/whl/torch_stable.html
 from audio_separator import Separator
 import sys
 import torch
